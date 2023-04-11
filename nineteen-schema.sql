46d7242937bc3e5fa7a9a788a586fe2078edaa5e
eggsandwich-dev <cpitre1@uncc.edu>
2023-04-11 13:40:05 -0400

    Added media table

--- nineteen-schema.sql
+++ nineteen-schema.sql
@@ -1,3 +1,11 @@
 DROP DATABASE IF EXISTS nineteen;
 CREATE DATABASE nineteen;
 USE nineteen;
+
+CREATE TABLE media (
+	media_id	INT NOT NULL,
+	media_type	VARCHAR(255) NOT NULL,
+	link	VARCHAR(255),
+
+	PRIMARY KEY (media_id)
+);
