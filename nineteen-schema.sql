1934590750858197b4ecb5c40bf97fae3b3b41a8
fadin47 <122924566+fadin47@users.noreply.github.com>
2023-04-11 13:45:55 -0400

    Update nineteen-schema.sql

--- nineteen-schema.sql
+++ nineteen-schema.sql
@@ -2,6 +2,14 @@ DROP DATABASE IF EXISTS nineteen;
 CREATE DATABASE nineteen;
 USE nineteen;
 
+CREATE TABLE IF NOT EXISTS User (
+    user_id INT AUTO_INCREMENT NOT NULL,
+    user_username VARCHAR(50) NOT NULL,
+    user_password VARCHAR(50) NOT NULL,
+    user_email VARCHAR(100) NOT NULL ,
+    PRIMARY KEY (user_id)
+);
+
 CREATE TABLE media (
 	media_id	INT NOT NULL,
 	media_type	VARCHAR(255) NOT NULL,
