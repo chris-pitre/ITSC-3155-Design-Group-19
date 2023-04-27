ab6e21aa9f0575866b1353c09885e16bcab4724f
eggsandwich-dev <cpitre1@uncc.edu>
2023-04-27 16:28:29 -0400

    created post page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+##temp data
 post1 = {
     "title": "Help with homework",
     "text": "i need help with this",
@@ -21,11 +22,37 @@ post2 = {
 }
 
 posts = []
+all_replies = []
 
 posts.append(post1)
 posts.append(post2)
-for i in range(10):
+for i in range(5):
+   posts.append(post1)
    posts.append(post2)
+
+post1reply = {
+    "parent_id": 0, 
+    "text": "look at this cow tho",
+    "post_date": "04/19/23",
+    "image": "https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg",
+    "alt_text": "cow"
+}
+
+post1reply2 = {
+    "parent_id": 0, 
+    "text": "cool",
+    "post_date": "04/19/23",
+    "image": None,
+    "alt_text": None
+}
+
+all_replies.append(post1reply)
+all_replies.append(post1reply2)
+
+def get_replies(post_id) -> list:
+   replies = [reply for reply in all_replies if post_id == reply["parent_id"]]
+   return replies
+## end of temp data
 
 @app.route('/')
 def home():
@@ -36,8 +63,10 @@ def home():
 
 @app.get("/forum")
 def forum_page():
-    return render_template("forum.html", posts=posts, total_posts = len(posts), total_replies = 0)
+    return render_template("forum.html", posts=enumerate(posts), total_posts=len(posts), total_replies=0)
 
-@app.get("/about")
-def about_page():
-   return render_template("About.html")
+@app.get("/post/<int:post_id>")
+def get_post(post_id):
+   post = posts[post_id]
+   replies = get_replies(post_id)
+   return render_template("post.html", post=post, replies=enumerate(replies))
